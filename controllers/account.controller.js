d4a8a85cc843827b690ea647e888cae00d112a25
Emmanuel Etukudo <emzzyyoung@gmail.com>
2021-03-26 19:46:09 +0100

    created account controller

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -1,1 +1,19 @@
 
+require("dotenv").config();
+const okra = require("npm-okrajs");
+const axios = require("axios");
+
+
+const account = {
+    get: async (req, res, next) =>{
+    
+        try {
+            const {account_no, account_name} = req.data;
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+module.exports = account;

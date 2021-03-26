ab64e3f3f3fc3346b90153f0aab38398af57d6af
Emmanuel Etukudo <emzzyyoung@gmail.com>
2021-03-26 19:55:51 +0100

    created .env

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -3,12 +3,14 @@ require("dotenv").config();
 const okra = require("npm-okrajs");
 const axios = require("axios");
 
-
 const account = {
     get: async (req, res, next) =>{
-    
         try {
             const {account_no, account_name} = req.data;
+            const base_url = process.env.api_url;
+            axios.get(`${base_url}/products/accounts`, 
+            
+            )
 
         } catch (error) {
             console.log(error);

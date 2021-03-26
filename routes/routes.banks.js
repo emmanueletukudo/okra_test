985da823230984c8136f0924b5f977d9f7c1204b
Emmanuel Etukudo <emzzyyoung@gmail.com>
2021-03-26 23:25:37 +0100

    update on banks controller

--- routes/routes.banks.js
+++ routes/routes.banks.js
@@ -3,6 +3,6 @@ const router = Router();
 
 const {account} =  require("../controllers/banks.controller");
 
-router.post("/banks", account.get);
+router.post("/banks", account.banks);
 
 module.exports = router;

831084170cfa775b1e570f51e6fc96bd696d4fc8
Emmanuel Etukudo <emzzyyoung@gmail.com>
2021-03-26 20:30:46 +0100

    update on account controller

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -1,21 +1,24 @@
-
 require("dotenv").config();
 const okra = require("okra-node");
 const axios = require("axios");
 
 const account = {
-    get: async (req, res, next) =>{
-        try {
-            const token = process.env.client_token;
-            const result =  okra.getCustomerByIdentity(token, {type: "BVN", value: "38261936382" }, (err, result) => {
-                if(err) return Error(err);
-                return result.identity;
-            });
-            res.json({data: result});
-        } catch (error) {
-            console.log(error);
+  get: async (req, res, next) => {
+    try {
+      const token = process.env.client_token;
+      const result = okra.getCustomerByIdentity(
+        token,
+        { type: "BVN", value: "38261936382" },
+        (err, result) => {
+          if (err) return Error(err);
+          return result.identity;
         }
+      );
+      res.json({ data: result });
+    } catch (error) {
+      console.log(error);
     }
-}
+  },
+};
 
-module.exports = {account};
+module.exports = { account };
